4fd8d557dcf25fff87ff0a14a1c560ccc3622d3a
Inna <inna@bmby.com>
2020-09-21 11:42:23 +0300

    added pages for webview

---
+++ gallery.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://bmbywidgetsqa.azurewebsites.net/gallery.js?canvas=bmby-gallery-wrapp&<?=$_GET['key']?>&type=<?=$_GET['type']?>&lang=<?=$_GET['lang']?>"></script>
+  <title>Document</title>
+</head>
+
+<body>
+<!--add class rtl to the this element to change items order,-->
+<!--for example <div class="bmby-about-us rtl">-->
+<div class="bmby-gallery-wrapp"></div>
+</body>
+</html>
