5afd4105dc9ef92edc3dd8f2d356ad5b6fffc5a7
Inna <inna@bmby.com>
2020-10-06 15:17:14 +0300

    added Odessa about developer production page

---
+++ galleryOdDev.php
@@ -0,0 +1,28 @@
+<?php
+$url = sprintf(
+  "%s://%s",
+  isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+  $_SERVER['SERVER_NAME']
+);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script
+        src="<?=$url?>/galleryOdDev.js?canvas=bmby-gallery-wrapp&lang=<?=$_GET['lang']?>"></script>
+
+    <title>Document</title>
+</head>
+
+<body>
+<!--add class rtl to the this element to change items order,-->
+<!--for example <div class="bmby-about-us rtl">-->
+<div class="bmby-gallery-wrapp"></div>
+</body>
+</html>
