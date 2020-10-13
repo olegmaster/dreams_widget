f19fb612080ce513be8a449ae34965816283bff2
Kolomiets Vitaliy <55140766+vertalik@users.noreply.github.com>
2020-10-13 18:00:20 +0300

    About Odessa , Gallery Odessa - routes

--- galleryOdessa.php
+++ galleryOdessa.php
@@ -15,7 +15,7 @@ $url = sprintf(
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script
-        src="<?=$url?>/galleryOd.js?canvas=bmby-gallery-wrapp&lang=<?=$_GET['lang']?>"></script>
+        src="<?=$url?>/galleryOdessa.js?canvas=bmby-gallery-wrapp&lang=<?=$_GET['lang']?>"></script>
 
     <title>Document</title>
 </head>
