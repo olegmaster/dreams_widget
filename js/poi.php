debad451b1709c2f2960a28e4307b0798b2e680c
Inna <inna@bmby.com>
2020-10-27 09:58:21 +0200

    add tmp poi files

---
+++ js/poi.php
@@ -0,0 +1,32 @@
+<?php
+require_once '../classes/WidgetApiInteractor.php';
+require_once '../classes/ApiHelper.php';
+require_once '../classes/GalleryJsGenerator.php';
+
+$canvasClass = $_GET['canvas'] ?? 'bmby-poi';
+
+$key = $_GET['key'] ?? '';
+$type = $_GET['type'] ?? '';
+$callback = $_GET['callback'] ?? '';
+$lang = $_GET['lang'] ?? 'en';
+
+$apiInteractor = new WidgetApiInteractor($_GET['key'] ?? '', $_GET['type'] ?? '');
+$galleryData = $apiInteractor->getGalleryApiData();
+
+$galleryCategoriesData = $apiInteractor->getGalleryCategoriesApiData();
+
+$galleryJsGenerator = new GalleryJsGenerator($galleryData, $canvasClass, $lang, $callback);
+
+$galleryJsGenerator->showJs();
+
+
+
+
+
+
+
+
+
+
+
+
