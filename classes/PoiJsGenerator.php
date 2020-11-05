b0222038c11061986334dec286d5749afd570438
Kolomiets Vitaliy <55140766+vertalik@users.noreply.github.com>
2020-11-05 16:54:02 +0200

    poi fix line 2

--- classes/PoiJsGenerator.php
+++ classes/PoiJsGenerator.php
@@ -1157,12 +1157,12 @@ function add_experimental_map (options) {
                                           (navigator.platform.indexOf("iPad") != -1) ||
                                           (navigator.platform.indexOf("iPod") != -1)) {
                                             //map_app_url = 'http://maps.apple.com/?ll=' + marker_position_lat +','+ marker_position_lng;
-                                            map_app_url = `https://www.google.com/maps?f=d&daddr=\${marker_position_lat +','+ marker_position_lng}`;
+                                            map_app_url = `https://www.google.com/maps?f=d&daddr=${marker_position_lat +','+ marker_position_lng}`;
                                             $(container).find('.links-btns-wrap').append(`<a class="map-place circle-btn" target="_blank" href="\${map_app_url}">\${get_lang('Navigate to')}</a>`);
 
                                         } else {
                                             // https://www.google.com/maps?f=d&saddr=Thompson+St,+New+York,+NY,+USA&daddr=Wooster+St,+New+York,+NY,+USA&dirflg=d
-                                            map_app_url = `https://www.google.com/maps?f=d&daddr=\${marker_position_lat +','+ marker_position_lng}`;
+                                            map_app_url = `https://www.google.com/maps?f=d&daddr=${marker_position_lat +','+ marker_position_lng}`;
                                             $(container).find('.links-btns-wrap').append(`<a class="map-place circle-btn" target="_blank" href="\${map_app_url}">\${get_lang('Navigate to')}</a>`);
                                         }
                                     })
