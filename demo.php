92e73df9478aa32e7cc1509f6289432a68f09b6e
Oleg Pristashkin <oleg.web.pro@gmail.com>
2020-11-16 16:15:01 +0200

    add canvas to the demo

--- demo.php
+++ demo.php
@@ -18,7 +18,7 @@
         <script src="js/gallery.php?key=4CEFC377-5791-41BD-9BB1-6E431F11F5D9&type=US"></script>
     <?php endif; ?>
     <?php if(isset($_GET['page']) && $_GET['page'] === 'poi'): ?>
-        <script src="js/poi.php?key=4CEFC377-5791-41BD-9BB1-6E431F11F5D9&type=US"></script>
+        <script src="js/poi.php?key=4CEFC377-5791-41BD-9BB1-6E431F11F5D9&type=US&canvas=bmby-poi"></script>
     <?php endif; ?>
     <title>Document</title>
 </head>
